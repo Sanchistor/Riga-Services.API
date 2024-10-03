6256006c83c14f2ab352b229d8fb2704aad62671
Oleksii Pecheniuk <aleksey.pecheniuk@icloud.com>
2024-10-03 20:27:34 +0300

    sanya !natural

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -37,22 +37,18 @@ public class AuthController : ControllerBase
     [HttpPost("Authorize")]
     public ActionResult<TokenResponse> AuthUser(UserLoginDto userLoginDto)
     {
-        // Fetch user from the database using the provided email
         User user = _context.Users.FirstOrDefault(u => u.Email == userLoginDto.email);
 
-        // Check if the user exists
         if (user == null)
         {
             return BadRequest(new { message = "User not found" });
         }
 
-        // Verify if the provided password matches the user's hashed password
         if (!_passwordService.VerifyPassword(userLoginDto.password, user.Password))
         {
             return Unauthorized(new { message = "Invalid password" });
         }
 
-        // If the password is correct, generate a JWT token for authentication
         var key = "Yh2k7QSu418CZg5p6X3Pna9L0Miy4D3Bvt0JVr87Uc0j69Kqw5R2Nmf4FWs03Hdx";
         JwtAuthenticationManager jwtAuthenticationManager = new JwtAuthenticationManager(key, _context);
         string token = jwtAuthenticationManager.Authenticate(user.Email, userLoginDto.password);
